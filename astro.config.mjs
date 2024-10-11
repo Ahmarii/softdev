12c6269755f6842f6310842d5e31b65f87b2370a
Ahmarii <ahmari.work@gmaiil.com>
2024-10-11 08:47:11 +0700

    test config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://ahmarii.github.io/',
+	base: 'softdev/',
 	integrations: [mdx(), sitemap()],
 });
