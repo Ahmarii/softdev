c95bd47ae576bf5a6a9715f70cb51d51450ee2ea
Ahmarii <ahmari.work@gmaiil.com>
2024-10-17 20:44:02 +0700

    fixed link

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,19 +9,4 @@ export default defineConfig({
 	site: 'https://ahmarii.github.io/',
 	base: isProd ? '/softdev/' : '/',
 	integrations: [mdx(), sitemap()],
-	markdown: {
-		remarkPlugins: [],
-		rehypePlugins: [],
-		extendDefaultPlugins: true,
-		imageAbsoluteUrls: true, // Allow absolute URLs
-		// Add the base path to Markdown links/images automatically
-		extendMarkdown(md) {
-		  const basePath = isProd ? '/softdev' : '';
-		  md.renderer.rules.image = (tokens, idx) => {
-			const token = tokens[idx];
-			const src = token.attrs[0][1];
-			return `<img src="${basePath}${src}" alt="${token.content}" />`;
-		  };
-		},
-	},
 });
