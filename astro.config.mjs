fe623263e3d7e09df92a760a437ad612eb9bf386
Ahmarii <ahmari.work@gmaiil.com>
2024-10-17 20:29:32 +0700

    prod image fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+const isProd = process.env.NODE_ENV === 'production';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ahmarii.github.io/',
-	base: process.env.NODE_ENV === 'development' ? '' : 'softdev/',
+	base: isProd ? '/softdev/' : '/',
 	integrations: [mdx(), sitemap()],
 });
