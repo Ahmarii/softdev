99cf557f621ccb04839d585e348335b1ebf3f383
Ahmarii <ahmari.work@gmaiil.com>
2024-10-17 20:38:59 +0700

    markdown fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,21 @@ const isProd = process.env.NODE_ENV === 'production';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ahmarii.github.io/',
-	base: 'softdev/',
+	base: isProd ? '/softdev/' : '/',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [],
+		rehypePlugins: [],
+		extendDefaultPlugins: true,
+		imageAbsoluteUrls: true, // Allow absolute URLs
+		// Add the base path to Markdown links/images automatically
+		extendMarkdown(md) {
+		  const basePath = isProd ? '/softdev' : '';
+		  md.renderer.rules.image = (tokens, idx) => {
+			const token = tokens[idx];
+			const src = token.attrs[0][1];
+			return `<img src="${basePath}${src}" alt="${token.content}" />`;
+		  };
+		},
+	},
 });
