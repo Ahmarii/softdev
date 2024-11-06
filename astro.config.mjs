a525661df972ced68fe48f1cb298bb703d485891
Ahmarii <ahmari.work@gmaiil.com>
2024-11-06 17:44:14 +0700

    Basic Cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 const isProd = process.env.NODE_ENV === 'production';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ahmarii.github.io/',
-	base: isProd ? '/softdev/' : '',
-	integrations: [mdx(), sitemap()],
+    site: 'https://ahmarii.github.io/',
+    base: isProd ? '/softdev/' : '',
+    integrations: [mdx(), sitemap(), tailwind()],
 });
